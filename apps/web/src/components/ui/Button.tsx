5ba18c96071b20f6821230f811d2cb35d44eb28b
silentesc <scope-frisk-copy@duck.com>
2026-09-25 22:42:38 +0200

    feat(web): add fullWidth and variant input params for button component

--- apps/web/src/components/ui/Button.tsx
+++ apps/web/src/components/ui/Button.tsx
@@ -2,17 +2,32 @@ interface ButtonProps extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>
     value: string | React.ReactElement;
     type?: "button" | "submit" | "reset";
     alignment?: "left" | "center" | "right";
+    fullWidth?: boolean;
+    variant?: "default" | "primary" | "danger";
     onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 }
 
-export function Button({ value, type = "button", alignment = "center", onClick = () => { }, ...props }: ButtonProps) {
+export function Button({ value, type = "button", alignment = "center", fullWidth = true, variant = "default", onClick = () => { }, ...props }: ButtonProps) {
+    let variantClassName = "";
+    switch (variant) {
+        case "default":
+            variantClassName = "border-background-tertiary bg-background-secondary hover:bg-background-tertiary";
+            break;
+        case "primary":
+            variantClassName = "border-primary bg-primary hover:bg-primary/80";
+            break;
+        case "danger":
+            variantClassName = "border-error/80 text-error bg-background-secondary hover:bg-error/15";
+            break;
+    }
     return (
         <>
             <button
                 onClick={onClick}
                 type={type}
                 className={
-                    `w-full border-background-tertiary border-2 p-2 outline-none bg-background-secondary hover:bg-background-tertiary hover:cursor-pointer 
+                    `${fullWidth ? "w-full" : "w-auto shrink-0"} border-2 p-2 outline-none hover:cursor-pointer
+                    ${variantClassName}
                     ${alignment === "left" && "text-left"}
                     ${alignment === "center" && "text-center"}
                     ${alignment === "right" && "text-right"}`
